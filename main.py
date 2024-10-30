5e2062580e222bf17d80587771d8a4f691dd2b02
Ali Hussain <ahalihussain11@gmail.com>
2024-10-30 15:02:37 +0500

    adding id number

--- main.py
+++ main.py
@@ -22,9 +22,17 @@ class order(BaseModel):
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2}]
 
-new_data = [{"name": "ali hussain", "order": "one large pizza", "location": "north nazimabad", "total": 1000}, {"name": "usman liaquat", "order": "beef burger with fries", "location": "gulshan e iqbal", "total": 1300}]
+new_data = [{"name": "ali hussain", "order": "one large pizza", "location": "north nazimabad", "total": 1000, "id": 3}, {"name": "usman liaquat", "order": "beef burger with fries", "location": "gulshan e iqbal", "total": 1300, "id": 4}]
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
+def send_data(id):
+    for p in new_data:
+        if p["id"] == id:
+            return p
 
 @app.get("/")
 def read_root():
@@ -46,9 +54,22 @@ def create_post(post: Post):
     my_posts.append(post_dict)
     return{"data": "post"}
 
+@app.get("/posts/{id}")
+def get_id(id: int):
+    print(id)
+    post = find_post(id)
+    return {"post-message": post}
+
 @app.post("/data_check")
 def data_check(data: order):
     data_dict = data.dict()
     data_dict['id'] = randrange(0, 10000)
     new_data.append(data_dict)
     return {"client_data": "new data"} 
+
+
+@app.get("/data_check/{id}")
+def data_id(id: int):
+    print(id)
+    data = send_data(id)
+    return {"post_message": data}
